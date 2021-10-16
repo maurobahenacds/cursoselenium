6dc33c7a71825c9c2c0cf82f1910a698ef9a22fa
maurobahenacds <84189162+maurobahenacds@users.noreply.github.com>
2021-10-16 14:48:03 -0500

    mmm

--- Selenium2021/src/main/java/selenium/SeleniumDemo.java
+++ Selenium2021/src/main/java/selenium/SeleniumDemo.java
@@ -11,7 +11,7 @@ public class SeleniumDemo {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");
 		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");
-		//comentaio de axctualizacion a las 2:45  jajaja
+		//comentaio de axctualizacion a las 2:45  jajaja mmmmm
 
 	}
 

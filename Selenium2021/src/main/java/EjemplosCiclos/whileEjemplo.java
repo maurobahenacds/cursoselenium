28efc5514859177e470a197c9021a0b4534945d4
maurobahenacds <84189162+maurobahenacds@users.noreply.github.com>
2021-10-23 09:37:47 -0500

    Actulización del 23 de octubre 2021

---
+++ Selenium2021/src/main/java/EjemplosCiclos/whileEjemplo.java
@@ -0,0 +1,16 @@
+package EjemplosCiclos;
+
+public class whileEjemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		while (a<=4) {//Salir del bucle cuando 'a' es mayor que 4
+			System.out.println("El valor de a: " + a);
+			a++;//inclrementa el valor de 'a' para la siguiente iteración
+			
+		}
+
+	}
+
+}
--- Selenium2021/src/main/java/EjemplosIF/Pruebas.java
+++ Selenium2021/src/main/java/EjemplosIF/Pruebas.java
@@ -1,4 +1,4 @@
-package selenium;
+package EjemplosIF;
 
 public class Pruebas {
 
